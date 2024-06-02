c1351435169431cf8925298bec154f29f55bcd95
gitnitinhub <nitin7817@gmail.com>
2024-06-02 21:52:22 +0530

    number string method added

---
+++ 03convertingdatatype.js
@@ -0,0 +1,17 @@
+// javascript allow us to convert from data types to another data type
+
+let score = 33
+
+console.log(typeof score) //number
+
+//to convert score to string we can 
+
+let convertscore = String(score)
+console.log(typeof convertscore) //string 
+
+//to convert convertscore from stirng to number 
+
+let numberscore = Number(convertscore)
+console.log(typeof numberscore) //number
+
+
