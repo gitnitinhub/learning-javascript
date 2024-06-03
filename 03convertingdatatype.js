5117a6af393470e790aa0913f347c447003ae225
gitnitinhub <nitin7817@gmail.com>
2024-06-03 10:39:18 +0530

    finalized the chapter

--- 03convertingdatatype.js
+++ 03convertingdatatype.js
@@ -29,3 +29,32 @@ conversions of Boolean
 "nitin" => true
 
 */
+
+// ******************************************** Operations ******************************************
+console.log("-------Operations topic start from here--------")
+
+
+let value = 3
+let negValue = -value
+
+console.log(negValue)  //output : -3
+
+let x = 6
+let y = ++x //prefix 
+let z = x++ //postfix
+
+console.log(`x is: ${x}, y is: ${y}, z is ${z}`) // output: x is: 8, y is: 7, z is 7
+
+let str1 = "Hello "
+let str2 = "Nitin"
+
+let str3 = str1 + str2
+console.log(str3) // concatination
+console.log(1+"2")   // output : '12' bcoz number + strig = string
+console.log(1+2+"2") // output : '32' bcoz number + number + string = number + string = string 
+
+/*
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+*/
