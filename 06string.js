3bcf37e7ad717ec428e04cc2a809b9eb7d284e6d
gitnitinhub <nitin7817@gmail.com>
2024-06-12 14:33:48 +0530

    string tut added

---
+++ 06string.js
@@ -0,0 +1,33 @@
+const name = "Nitin";
+const repocount = 10;
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`)
+
+//Methods in strings
+
+let string = "mynameisnitin"
+
+console.log(string[1])                  //output: y
+console.log(string.toUpperCase())       //output: MYNAMEISNITIN
+console.log(string.charAt(2))           //output : n
+console.log(string.indexOf('n'))        //output: 2
+
+//substring and slice method of string
+
+//sbustirng
+string = "mynameisnitin"
+const newstring = string.substring(0,4) //output: myna [takes only positive value. 0,4 are indexs to create substring form 0 to 4 where 4 will not be included] 
+console.log(newstring)
+
+
+//slice
+const slicepstring = string.slice(0,4)  //output: myna [ 0,4 are indexs to create slice form 0 to 4 where 4 will not be included]
+console.log(slicepstring)                
+const slicenstring = string.slice(1,-4) //output: ynameisn [1,-4 here -4 is counting from last of string and 1 is counting from start of string]
+console.log(slicenstring)
+
+//trim
+let nstring = "              stringwithspaceatbothend               "
+console.log(nstring.trim())             //output: stringwithspaceatbothend [trim remove spaces from both the end]
+console.log(nstring.trimStart())        //output: stringwithspaceatbothend               [trim start remove space from start only]
+console.log(nstring.trimEnd())          //output:               stringwithspaceatbothend [trim end remove space from end only]
--- practice.js
+++ practice.js
@@ -1,9 +1,36 @@
-// find a prime number
+//find duplicate elements in array in javascript 
 
-let arr = [1,2,3,4,5,6,7]
+let arr= [1,2,3,3,4,5,6,7,7]
+let duplicates=[]
 
-for (let i = 0  ; i < arr.length; i++){
-    if (arr[i] <=1){
-        console.log('Not a prime number')
-    } 
+function findduplicates(arr){
+for (let i=0;i<arr.length;i++){
+    for(let j=i+1;j<arr.length;j++){
+        if (arr[i]=== arr[j] && !duplicates.includes(arr[i])){
+            duplicates.push(arr[i])
+        }
+    }
+
 }
+console.log('Duplicates element in the array are: '+ duplicates)
+}
+
+findduplicates(arr)
+
+//how to find max and min value in a given array in js ?
+
+let marr = [3,5,6,1,4,8,9,10,12]
+
+let max = marr[0]
+let min = marr[0]
+
+for(let i=1; i<marr.length; i++){
+    if (min > marr[i]){
+        min = marr[i]
+    }
+    if (max < marr[i]){
+        max = marr[i]
+    }
+
+} console.log(min,max)
+
